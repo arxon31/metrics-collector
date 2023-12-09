912bbc1e9b45e2858fe52d625d8db215936528cc
arxon31 <arxon31@gmail.com>
2023-12-09 21:27:18 +0300

    Fixed linter errors, fixed tests

--- internal/handlers/postmetric_test.go
+++ internal/handlers/postmetric_test.go
@@ -2,13 +2,14 @@ package handlers
 
 import (
 	"github.com/arxon31/metrics-collector/internal/storage/mem"
+	"github.com/go-chi/chi/v5"
 	"github.com/stretchr/testify/assert"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
 
-func TestGaugeHandler(t *testing.T) {
+func TestPostMetrics(t *testing.T) {
 	type want struct {
 		statusCode  int
 		contentType string
@@ -20,7 +21,7 @@ func TestGaugeHandler(t *testing.T) {
 		want     want
 	}{
 		{
-			name:     "Successful request",
+			name:     "Successful request[gauge]",
 			endpoint: "http://localhost:8080/update/gauge/testMetric/10.0001",
 			want: want{
 				statusCode:  http.StatusOK,
@@ -28,36 +29,56 @@ func TestGaugeHandler(t *testing.T) {
 			},
 		},
 		{
-			name:     "Without metric name",
+			name:     "Without metric name[gauge]",
 			endpoint: "http://localhost:8080/update/gauge/10.0001",
 			want: want{
 				statusCode:  http.StatusNotFound,
 				contentType: "text/plain; charset=utf-8",
 			},
 		}, {
-			name:     "Without metric value",
-			endpoint: "http://localhost:8080/update/gauge/testMetric/",
+			name:     "With unsupported metric value[gauge]",
+			endpoint: "http://localhost:8080/update/gauge/testMetric/value",
 			want: want{
 				statusCode:  http.StatusBadRequest,
 				contentType: "text/plain; charset=utf-8",
 			},
 		}, {
-			name:     "With unsupported metric value",
-			endpoint: "http://localhost:8080/update/gauge/testMetric/value",
+			name:     "Successful request [counter]",
+			endpoint: "http://localhost:8080/update/counter/testMetric/10",
+			want: want{
+				statusCode:  http.StatusOK,
+				contentType: "text/plain; charset=utf-8",
+			},
+		},
+		{
+			name:     "Without metric name [counter]",
+			endpoint: "http://localhost:8080/update/counter/10",
+			want: want{
+				statusCode:  http.StatusNotFound,
+				contentType: "text/plain; charset=utf-8",
+			},
+		}, {
+			name:     "With unsupported metric value [counter]",
+			endpoint: "http://localhost:8080/update/counter/testMetric/value",
 			want: want{
 				statusCode:  http.StatusBadRequest,
 				contentType: "text/plain; charset=utf-8",
 			},
 		},
 	}
+
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+
 			req := httptest.NewRequest(http.MethodPost, tt.endpoint, nil)
 			w := httptest.NewRecorder()
 
-			var gauge PostMetrics
-			gauge.Storage = mem.NewMapStorage()
-			gauge.ServeHTTP(w, req)
+			var postMetrics PostMetrics
+			postMetrics.Storage = mem.NewMapStorage()
+
+			router := chi.NewRouter()
+			router.Post("/update/{type}/{name}/{value}", postMetrics.ServeHTTP)
+			router.ServeHTTP(w, req)
 
 			res := w.Result()
 			defer res.Body.Close()
